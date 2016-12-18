484cc818591b7b072d9a4ff0047d984c9467a2fe
Alexey Maltsev <axel.user.perm@gmail.com>
2016-12-18 16:54:49 +0500

    Project init

---
+++ TaskManager/TaskManager.Portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaskManager.Portal.WebApiApplication" Language="C#" %>
---
+++ TaskManager/TaskManager.ServiceBus.WinService/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.ServiceBus.WinService
+{
+    public class Class1
+    {
+    }
+}
