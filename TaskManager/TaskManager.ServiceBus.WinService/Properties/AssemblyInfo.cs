ad50f6f997b58435fa1ef791de999da94700a045
Alexey Maltsev <axel.user.perm@gmail.com>
2016-12-18 17:56:30 +0500

    Fixed WinService init

--- TaskManager/TaskManager.ServiceBus.WinService/Properties/AssemblyInfo.cs
+++ TaskManager/TaskManager.ServiceBus.WinService/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("12b69220-6d95-4e73-a125-b718c3de8836")]
+[assembly: Guid("fe3a5b21-e346-4607-bbbd-a3cea2b8dfc1")]
 
 // Version information for an assembly consists of the following four values:
 //
