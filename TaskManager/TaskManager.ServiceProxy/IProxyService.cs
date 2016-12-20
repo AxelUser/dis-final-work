bda872c1ad4e1faa9eda3126a9fd3ea308e11ff9
Alexey Maltsev <axel.user.perm@gmail.com>
2016-12-21 00:16:45 +0500

    Init service proxy, added RabbitMQ

---
+++ TaskManager/TaskManager.ServiceProxy/IProxyService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TaskManager.ServiceProxy
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IProxyService
+    {
+
+        [OperationContract]
+        void Report(int projectId);
+
+        [OperationContract]
+        void Notify(int taskId);
+    }
+}
---
+++ TaskManager/TaskManager.ServiceProxy/ProxyService.svc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace TaskManager.ServiceProxy
+{
+    public class ProxyService : IProxyService
+    {
+        private IConnection connection;
+        readonly string qNotify = "q_notify";
+        readonly string qReport = "q_report";
+        public ProxyService()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+        }
+        public void Notify(int taskId)
+        {
+            using(IModel channel = connection.CreateModel())
+            {
+                var props = channel.CreateBasicProperties();
+                props.Headers.Add("task-id", taskId.ToString());
+                
+                channel.QueueDeclare(
+                    queue: qNotify,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                byte[] body = BitConverter.GetBytes(taskId);
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: qNotify,
+                    basicProperties: props);
+            }
+        }
+
+        public void Report(int projectId)
+        {
+            using (IModel channel = connection.CreateModel())
+            {
+                var props = channel.CreateBasicProperties();
+                props.Headers.Add("task-id", projectId.ToString());
+                channel.QueueDeclare(
+                    queue: qReport,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                byte[] body = BitConverter.GetBytes(projectId);
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: qReport,
+                    basicProperties: null,
+                    body: body);
+            }
+        }
+    }
+}
