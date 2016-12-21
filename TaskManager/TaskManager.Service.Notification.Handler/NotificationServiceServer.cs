8e5d6b153fe8d0b9dc6d766dc069dcf9a77785ba
Alexey Maltsev <axel.user.perm@gmail.com>
2016-12-21 18:07:36 +0500

    Init Notification Service

---
+++ TaskManager/TaskManager.Service.Notification.Handler/NotificationServiceServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Service.Notification.Handler
+{
+    public class NotificationServiceServer
+    {
+    }
+}
