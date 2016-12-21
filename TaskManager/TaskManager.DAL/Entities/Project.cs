b73fb2fd67ee5a722880373a0042bd8e3826f3b9
Alexey Maltsev <axel.user.perm@gmail.com>
2016-12-22 04:41:50 +0500

    Fix properties

--- TaskManager/TaskManager.DAL/Entities/Project.cs
+++ TaskManager/TaskManager.DAL/Entities/Project.cs
@@ -15,6 +15,6 @@ namespace TaskManager.DAL.Entities
         public string Title { get; set; }
         public int UserId { get; set; }
         public virtual User User { get; set; }
-        public virtual IEnumerable<ProjectTask> Tasks { get; set; }
+        public virtual IEnumerable<ProjectTask> ProjectTasks { get; set; }
     }
 }
